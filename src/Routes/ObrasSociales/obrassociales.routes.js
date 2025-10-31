56a945f84a422d7c0f69d9f07f98e716b29cdd66
KarenLHerrera <karenherrera.dev@gmail.com>
2025-10-30 22:16:34 -0300

    se agregan validaciones al crear y modificar una obra social

--- src/Routes/ObrasSociales/obrassociales.routes.js
+++ src/Routes/ObrasSociales/obrassociales.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 //importo las funciones del controlador
-import { actualizarObraSocial, borradoLogicoObraSocial, crearObraSocial, obtenerObraSocialPorId, obtenerObrasSociales } from '../../Controllers/ObrasSociales/obrassociales.controller.js';
+import { actualizarObraSocial, borradoLogicoObraSocial, crearObraSocial, obtenerObraSocialActiva, obtenerObraSocialInactiva, obtenerObraSocialPorId, obtenerObrasSociales } from '../../Controllers/ObrasSociales/obrassociales.controller.js';
 
 const router = Router();
 
@@ -10,6 +10,8 @@ router.get('/:idObraSocial', obtenerObraSocialPorId);
 router.post('/crearObraSocial', crearObraSocial);
 router.put('/actualizarObraSocial/:idObraSocial', actualizarObraSocial);
 router.put('/borrarObraSocial/:idObraSocial', borradoLogicoObraSocial);
+router.get('/activos', obtenerObraSocialActiva);
+router.get('/inactivos', obtenerObraSocialInactiva);
 
 
 export default router;
