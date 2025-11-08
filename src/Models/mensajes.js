58a6bcb969649d34875d20e272fd8cd93f5e6b5c
KarenLHerrera <karenherrera.dev@gmail.com>
2025-11-08 15:38:37 -0300

    mensajeria interna

--- src/Models/mensajes.js
+++ src/Models/mensajes.js
@@ -63,16 +63,21 @@ const mensajesInternos = {
         n.Leido,
         n.idRemitente,
         u1.MailUsuario AS RemitenteEmail,
-        e.NombreEmpleado AS NombreDestinatario,
-        e.ApellidoEmpleado AS ApellidoDestinatario,
+        e_remitente.idEmpleado AS idEmpleadoRemitente,
+        e_remitente.NombreEmpleado AS NombreRemitente,
+        e_remitente.ApellidoEmpleado AS ApellidoRemitente,
+        e_destinatario.idEmpleado AS idEmpleadoDestinatario,
+        e_destinatario.NombreEmpleado AS NombreDestinatario,
+        e_destinatario.ApellidoEmpleado AS ApellidoDestinatario,
         nd.idEmpleadoDestinatario
       FROM notificaciones n
       JOIN notificaciones_destinatarios nd ON n.idNotificacion = nd.idNotificacion
       JOIN usuarios u1 ON n.idRemitente = u1.idUsuario
-      JOIN empleados e ON nd.idEmpleadoDestinatario = e.idEmpleado
+      LEFT JOIN empleados e_remitente ON n.idRemitente = e_remitente.idUsuario
+      JOIN empleados e_destinatario ON nd.idEmpleadoDestinatario = e_destinatario.idEmpleado
       WHERE 
-        (n.idRemitente = ? AND nd.idEmpleadoDestinatario = ?) OR
-        (n.idRemitente = ? AND nd.idEmpleadoDestinatario = ?)
+        (n.idRemitente = ? AND nd.idEmpleadoDestinatario IN (SELECT idEmpleado FROM empleados WHERE idUsuario = ?)) OR
+        (n.idRemitente = ? AND nd.idEmpleadoDestinatario IN (SELECT idEmpleado FROM empleados WHERE idUsuario = ?))
       ORDER BY n.FechaEnvio ASC
     `;
     db.query(sql, [idUser1, idUser2, idUser2, idUser1], (err, results) => {
--- src/Routes/Empleados/empleados.routes.js
+++ src/Routes/Empleados/empleados.routes.js
@@ -5,17 +5,18 @@ import { actualizarEmpleado, buscarEmpleadoPorDNI, buscarEmpleadosPorApellido, b
 
 const router = Router();
 
-// Rutas específicas primero
+
 router.get("/", obtenerEmpleados);
+router.post("/crearEmpleado", crearEmpleado);
 router.get("/activos", obtenerEmpleadosActivos);
 router.get("/inactivos", obtenerEmpleadosInactivos);
 router.get("/buscar/dni/:DNI", buscarEmpleadoPorDNI);
 router.get("/buscar/nombre/:NombreEmpleado", buscarEmpleadosPorNombre);
 router.get("/buscar/apellido/:ApellidoEmpleado", buscarEmpleadosPorApellido);
-router.post("/", crearEmpleado);
-router.put("/cambiar-estado/:idEmpleado", cambiarEstadoEmpleado);
-router.put("/:idEmpleado", actualizarEmpleado);
-// Ruta dinámica al final
+
+router.put("/actualizarEmpleado/:idEmpleado", actualizarEmpleado);
+router.put("/cambiarestado/:idEmpleado", cambiarEstadoEmpleado);
+
 router.get("/:idEmpleado", obtenerEmpleadoPorId);
 
 export default router;
