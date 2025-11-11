f9df78e1ce1f0875530d1848c67b0ad6a2d8af3a
KarenLHerrera <karenherrera.dev@gmail.com>
2025-11-10 23:06:45 -0300

    comentario

--- src/Middlewares/validation.js
+++ src/Middlewares/validation.js
@@ -2,6 +2,7 @@ import { crearComentarioSchema } from "../models/comentario.schema.js";
 import { actualizarComentarioSchema } from "../models/comentario.schema.js";
 
 // Middleware para validar datos al crear un comentario
+// Utiliza el esquema crearComentarioSchema
 export const validarCrearComentario = (req, res, next) => {
   const { error } = crearComentarioSchema.validate(req.body);
 
